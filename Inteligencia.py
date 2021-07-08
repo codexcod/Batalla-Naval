84476fc69f0bdf64a7092c2e90aeae09e5a4f7d9
Joaco <nunezjoac@gmail.com>
2021-07-08 01:30:23 -0300

    casi funciona

--- Inteligencia.py
+++ Inteligencia.py
@@ -15,56 +15,70 @@ class Robot:
         self.aseguradas = []
 
     def disparar(self,tablero):
-        print(f""" aseguradas:{len(self.aseguradas)} 
-                    micro : {len(self.microPosibilidades)}
-                    posibilidades = {len(self.posibilidades)}""")
+
         if self.barcoEnMira:
-            if not len(self.aseguradas) > 0:
-                if not len(self.microPosibilidades) > 0:
+            if len(self.aseguradas) == 0:
+                if  len(self.microPosibilidades) == 0:
                     if len(self.posibilidades) == 4 or len(self.posibilidades) == 3:
                         aleatorio =random.randrange(0,len(self.posibilidades))
+                        print(f"disparar a {self.posibilidades[aleatorio].x} {self.posibilidades[aleatorio].y}")
                         if tablero.dispararPunto(self.posibilidades[aleatorio].x,self.posibilidades[aleatorio].y):
                             if self.mira.x ==  self.posibilidades[aleatorio].x:
-                                diferencia = self.posibilidades[aleatorio].x - self.mira.x
-                                self.posibilidades.clear()
-                                self.microPosibilidades.append(Celda(self.mira.x - diferencia,self.mira.y))
-                                self.microPosibilidades.append(Celda(self.mira.x + diferencia * 2,self.mira.y))
-
-                            else:
                                 diferencia = self.posibilidades[aleatorio].y - self.mira.y
+                                print(f"La diferencia es {diferencia}")
                                 self.posibilidades.clear()
                                 self.microPosibilidades.append(Celda(self.mira.x,self.mira.y - diferencia))
                                 self.microPosibilidades.append(Celda(self.mira.x,self.mira.y + diferencia * 2))
+
+                            else:
+                                diferencia = self.posibilidades[aleatorio].x - self.mira.x
+                                print(f"La diferencia es {diferencia}")
+                                self.posibilidades.clear()
+                                self.microPosibilidades.append(Celda(self.mira.x - diferencia,self.mira.y ))
+                                self.microPosibilidades.append(Celda(self.mira.x + diferencia * 2,self.mira.y ))
 
                         else:
                             self.posibilidades.remove(self.posibilidades[aleatorio])
 
                     elif len(self.posibilidades) == 2:
                         aleatorio =random.randrange(0,2)
+                        print(f"disparar a {self.posibilidades[aleatorio].x} {self.posibilidades[aleatorio].y}")
                         if tablero.dispararPunto(self.posibilidades[aleatorio].x,self.posibilidades[aleatorio].y):
                             if self.mira.x ==  self.posibilidades[aleatorio].x:
-                                diferencia = self.posibilidades[aleatorio].x - self.mira.x
-                                self.posibilidades.clear()
-                                self.aseguradas.append(Celda(self.mira.x - diferencia,self.mira.y))
-
-                            else:
                                 diferencia = self.posibilidades[aleatorio].y - self.mira.y
                                 self.posibilidades.clear()
-                                self.aseguradas.append(Celda(self.mira.x,self.mira.y - diferencia))
+                                self.aseguradas.append(Celda(self.mira.x ,self.mira.y + diferencia))
+
+                            else:
+                                diferencia = self.posibilidades[aleatorio].x - self.mira.x
+                                self.posibilidades.clear()
+                                self.aseguradas.append(Celda(self.mira.x  + diferencia,self.mira.y))
 
                         else:
                             self.aseguradas.append(self.posibilidades[0])
-                            self.posibilidades.clear()
+                            if self.mira.x ==  self.posibilidades[0].x:
+                                diferencia = self.posibilidades[0].y - self.mira.y
+                                self.posibilidades.clear()
+                                self.aseguradas.append(Celda(self.mira.x ,self.mira.y - diferencia * 2))
+
+                            else:
+                                diferencia = self.posibilidades[0].x - self.mira.x
+                                self.posibilidades.clear()
+                                self.aseguradas.append(Celda(self.mira.x - diferencia * 2,self.mira.y ))
+
                 else:
                     aleatorio = random.randrange(0,2)
+                    print(f" aleatorio es {aleatorio}")
+                    print(f"disparar a {self.microPosibilidades[aleatorio].x} {self.microPosibilidades[aleatorio].y}")
                     if tablero.dispararPunto(self.microPosibilidades[aleatorio].x,self.microPosibilidades[aleatorio].y):
                         self.barcoEnMira= False
                         self.mira = None
                     else:
-                        self.microPosibilidades.remove(self.microPosibilidades[aleatorio])
+                        self.microPosibilidades.clear()
                         self.aseguradas.append(self.microPosibilidades[0])
 
             else:
+                print(f"disparar a {self.aseguradas[len(self.aseguradas) - 1].x} {self.aseguradas[len(self.aseguradas) - 1 ].y}")
                 tablero.dispararPunto(self.aseguradas[len(self.aseguradas) - 1].x,self.aseguradas[len(self.aseguradas) - 1 ].y)
                 if len(self.aseguradas) == 1:
                     self.barcoEnMira= False
@@ -88,6 +102,12 @@ class Robot:
                 self.posibilidades.append(Celda(x+1,y))
                 self.posibilidades.append(Celda(x,y-1))
                 self.posibilidades.append(Celda(x-1,y))
+
+        print(f""" 
+        {self.barcoEnMira}
+        aseguradas:{len(self.aseguradas)} 
+                            micro : {len(self.microPosibilidades)}
+                            posibilidades = {len(self.posibilidades)}""")
 
 
     
